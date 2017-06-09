c1db1df219405795c6a5bedc68b593c229569d62
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-06-09 16:04:17 -0600

    ADD MANY BOOKS.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import {createStore} from 'redux';
 
 //STEP 3 define reducers
-const reducer = (state={}, action) => {
+const reducer = (state=[], action) => {
     switch(action.type) {
         case "POST_BOOK":
-            return state = action.payload;
+            let books = state.concat(action.payload);
+            return books;
     }
 
     return state;
@@ -17,16 +18,33 @@ const store = createStore(
 
 store.subscribe(() => {
     console.log('Current state is: ', store.getState());
-    console.log('Current price is: ' + store.getState().price);
 });
 
 // STEP 2 create and dispatch actions
 store.dispatch({
     type:"POST_BOOK",
-    payload: {
+    payload: 
+    [{
         id: 1,
         title:'this is the book title',
         description: 'this is the book description',
         price: 33.33
+    },    
+    {
+        id: 2,
+        title:'this is the second book title',
+        description: 'this is the second book description',
+        price: 50
+    }]            
+});
+
+// DISPATCH another book
+store.dispatch({
+    type:"POST_BOOK",
+    payload: {
+        id: 3,
+        title:'this is the third book title',
+        description: 'this is the third book description',
+        price: 40
     }
 });
